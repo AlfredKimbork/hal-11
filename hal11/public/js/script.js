86e7ad04a04840002e898d29d6507dd40c5c9ee6
Alfred K. <107874027+AlfredKimbork@users.noreply.github.com>
2025-11-19 10:21:36 +0100

    page navigation added
    see where on page you are and click to move

--- hal11/public/js/script.js
+++ hal11/public/js/script.js
@@ -1,2 +1,49 @@
 "use strict";
-console.log("hello world");
+const sections = document.querySelectorAll(".main__section");
+const pageNav = document.querySelector("#page-nav ul");
+let pageNavItems;
+const toggleAriaCurrent = (anchor) => {
+    anchor.getAttribute("aria-current") == "true" ?
+        anchor.setAttribute("aria-current", 'false')
+        : anchor.setAttribute("aria-current", 'true');
+};
+// create anchor
+pageNavItems = Array.from(sections).map((section, i) => {
+    let pageNavItem = document.createElement("li");
+    pageNavItem.innerHTML = `<a href="#section-${i + 1}">hello</a>`;
+    const anchor = pageNavItem.firstChild;
+    pageNavItem.setAttribute("id", `dot-${i + 1}`); // give anchor a class to access them later
+    anchor.setAttribute("aria-current", 'true'); // give it accebility feature
+    // pageNavItem.firstChild!.addEventListener("click", e => {
+    //     pageNavItems.forEach(() => {
+    //         const target = e.target as HTMLAnchorElement;
+    //         console.log(target)
+    //         anchor.setAttribute("aria-current", 'false');
+    //         target.setAttribute("aria-current", 'true');
+    //     })
+    // });
+    pageNav.append(pageNavItem);
+    // show position and disable prevBtn
+    i == 0 ? toggleAriaCurrent(anchor) : "";
+    return pageNavItem;
+});
+let i = 0;
+const scrollAnimation = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+        var _a;
+        entry.target.classList.toggle("show", entry.isIntersecting);
+        const id = (_a = entry.target.getAttribute("id")) === null || _a === void 0 ? void 0 : _a.slice(8);
+        console.log(id);
+        pageNavItems.forEach(pageNavItem => {
+            const anchor = pageNavItem.firstChild;
+            if (pageNavItem.id == `dot-${id}`) {
+                toggleAriaCurrent(anchor);
+            }
+        });
+    });
+}, {
+    threshold: .6,
+});
+sections.forEach(section => {
+    scrollAnimation.observe(section);
+});
